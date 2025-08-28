776a564f97876fbd0b29ed1b8122887227074a8a
Felix Staacke <felix.staacke@uni-jena.de>
2025-08-28 20:01:45 +0200

    updated SST

--- tools/tools.manifest.js
+++ tools/tools.manifest.js
@@ -1,3 +1,4 @@
 window.TOOLS_MANIFEST = [
-    { file: "markdownGenerator.html", label: "Markdown-Generator" }
+    { file: "markdownGenerator.html", label: "Markdown-Generator" },
+    { file: "SST.html", label: "Scrum 4 School" }
 ];
